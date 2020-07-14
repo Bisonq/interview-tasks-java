8ea23bde8831b03479c568047d9c082020b1c055
Tomasz Sosnowski <tsosnowski666@gmail.com>
2020-07-14 17:30:19 +0200

    task1 - first version of the implementation of findOddNumbers method

--- src/main/java/eu/codeloop/task1/Numbers.java
+++ src/main/java/eu/codeloop/task1/Numbers.java
@@ -1,6 +1,9 @@
 package eu.codeloop.task1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class Numbers {
 
@@ -8,6 +11,29 @@ public final class Numbers {
     }
 
     public static List<Integer> findOddNumbers(List<String> numbers) {
-        throw new UnsupportedOperationException("TODO");
+        if(numbers == null)
+            throw new IllegalArgumentException("Given argument cannot be null!");
+
+        List<Integer> resultList = new ArrayList<>();
+
+        if(numbers.isEmpty())
+            return resultList;
+
+        for(String numberSequence : numbers){
+            if(isSequenceOfNumbers(numberSequence)){
+                int extractedNumber = Integer.parseInt(numberSequence);
+                if(extractedNumber % 2 != 0)
+                    resultList.add(extractedNumber);
+            }
+        }
+        return resultList;
+    }
+
+    protected static boolean isSequenceOfNumbers(String sequence){
+        if(sequence == null || sequence.length() < 1)
+            return false;
+        Pattern pattern = Pattern.compile("-?\\d+");
+        Matcher matcher = pattern.matcher(sequence);
+        return matcher.matches();
     }
 }
