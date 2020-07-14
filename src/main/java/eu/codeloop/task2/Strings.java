d1d422176057ac92ae17e6cf21b8fc4298eba793
Tomasz Sosnowski <tsosnowski666@gmail.com>
2020-07-14 19:07:53 +0200

    task2 - first version of hasDuplicateChars method implementation

--- src/main/java/eu/codeloop/task2/Strings.java
+++ src/main/java/eu/codeloop/task2/Strings.java
@@ -1,11 +1,25 @@
 package eu.codeloop.task2;
 
+import java.util.Arrays;
+
 public final class Strings {
 
     private Strings() {
     }
 
     public static boolean hasDuplicateChars(String text) {
-        throw new UnsupportedOperationException("TODO");
+        if(text == null)
+            throw new IllegalArgumentException("Given argument cannot be null!");
+
+        if(text.length() < 2)
+            return false;
+
+        char[] charArray = text.toCharArray();
+        Arrays.sort(charArray);
+
+        for(int i = 0 ; i < charArray.length - 1 ; i++)
+            if(charArray[i] == charArray[i+1])
+                return true;
+        return false;
     }
 }
