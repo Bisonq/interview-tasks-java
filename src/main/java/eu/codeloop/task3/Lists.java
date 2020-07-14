4d8725ba30acef500cae96084fc69774bf563c53
Tomasz Sosnowski <tsosnowski666@gmail.com>
2020-07-14 22:49:11 +0200

    task3 - first version of mapBy/mapByV2 method implementation

--- src/main/java/eu/codeloop/task3/Lists.java
+++ src/main/java/eu/codeloop/task3/Lists.java
@@ -1,8 +1,10 @@
 package eu.codeloop.task3;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public final class Lists {
 
@@ -10,6 +12,25 @@ public final class Lists {
     }
 
     public static <K, V> Map<K, V> mapBy(Collection<V> collection, Function<? super V, ? extends K> mapper) {
-        throw new UnsupportedOperationException("TODO");
+        if(collection == null || mapper == null)
+            throw new IllegalArgumentException();
+
+        Map<K, V> resultMap = new HashMap<>();
+
+        if(collection.isEmpty())
+            return resultMap;
+
+        for(V value : collection)
+            resultMap.put(mapper.apply(value), value);
+
+        return resultMap;
+    }
+
+    public static <K, V> Map<K, V> mapByV2(Collection<V> collection, Function<? super V, ? extends K> mapper) {
+        if(collection == null || mapper == null)
+            throw new IllegalArgumentException();
+        if(collection.isEmpty())
+            return new HashMap<>();
+        return collection.stream().collect(Collectors.toMap(mapper, Function.identity(), (vOld, vNew) -> vNew));
     }
 }
